40c75eaa352f29c76ad5fe434825298d7dda2df1
depohmel <ilya@kasten.io>
2018-12-04 11:28:41 -0800

    i command you to work

--- restore.sh
+++ restore.sh
@@ -9,15 +9,7 @@ set -o xtrace
 target="${1}"
 ns="kasten-io"
 
-alias kubectl='docker run --rm -ti -p 5432 -p 9000-9030 -p 41134 --volumes-from $(hostname) -e HOME=${SHIPPABLE_BUILD_DIR} -e KUBECONFIG=${SHIPPABLE_BUILD_DIR}/kubectl_cfg lachlanevenson/k8s-kubectl:v1.11.5'
-
-kubectl get restorepoints -n "${ns}" -o yaml
-
-kubectl version
-
-helm status k10
-
-backup_name=$(kubectl get restorepoints -n "${ns}" -ojson | jq ' .items | sort_by(.metadata.creationTimestamp) | .[-1] | .metadata.name ')
+backup_name=$(docker run --rm -ti -p 5432 -p 9000-9030 -p 41134 --volumes-from $(hostname) -e HOME=${SHIPPABLE_BUILD_DIR} -e KUBECONFIG=${SHIPPABLE_BUILD_DIR}/kubectl_cfg lachlanevenson/k8s-kubectl:v1.11.5 get restorepoints -n "${ns}" -ojson | jq ' .items | sort_by(.metadata.creationTimestamp) | .[-1] | .metadata.name ')
 
 cat << EOF | kubectl create -n ${ns} -f -
 apiVersion: actions.kio.kasten.io/v1alpha1
@@ -36,13 +28,13 @@ spec:
     pointInTime: 2030-01-01T00:00:00Z
 EOF
 
-restore_name=$(kubectl get restoreactions -n "${ns}" -ojson | jq -r ' .items | sort_by(.metadata.creationTimestamp) | .[-1] | .metadata.name ')
+restore_name=$(docker run --rm -ti -p 5432 -p 9000-9030 -p 41134 --volumes-from $(hostname) -e HOME=${SHIPPABLE_BUILD_DIR} -e KUBECONFIG=${SHIPPABLE_BUILD_DIR}/kubectl_cfg lachlanevenson/k8s-kubectl:v1.11.5 get restoreactions -n "${ns}" -ojson | jq -r ' .items | sort_by(.metadata.creationTimestamp) | .[-1] | .metadata.name ')
 
 state=""
 while [[ "${state}" != "Passed" ]] && [[ "${state}" != "Failed" ]]
 do
     sleep 3
-    state=$(kubectl get restoreactions -n "${ns}" "${restore_name}" -ojson | jq -r '.status.state')
+    state=$(docker run --rm -ti -p 5432 -p 9000-9030 -p 41134 --volumes-from $(hostname) -e HOME=${SHIPPABLE_BUILD_DIR} -e KUBECONFIG=${SHIPPABLE_BUILD_DIR}/kubectl_cfg lachlanevenson/k8s-kubectl:v1.11.5 get restoreactions -n "${ns}" "${restore_name}" -ojson | jq -r '.status.state')
 done
 
 if [[ "${state}" == "Failed" ]]

d98effa3b9b701d0abf5dc0860c64df41f11b654
Tom Manville <tom@kasten.io>
2018-12-04 01:33:02 -0800

    Restore latest restorepoint in CI

--- restore.sh
+++ restore.sh
@@ -7,10 +7,11 @@ set -o xtrace
 
 
 target="${1}"
+ns="kasten-io"
 
-backup=$(kubectl get restorepoints -n kasten-io -ojson | jq ' .items | sort_by(.metadata.creationTimestamp) | .[-1] | .metadata.name ')
+backup_name=$(kubectl get restorepoints -n "${ns}" -ojson | jq ' .items | sort_by(.metadata.creationTimestamp) | .[-1] | .metadata.name ')
 
-cat << EOF | kubectl create -f -
+cat << EOF | kubectl create -n ${ns} -f -
 apiVersion: actions.kio.kasten.io/v1alpha1
 kind: RestoreAction
 metadata:
@@ -20,12 +21,23 @@ spec:
     subject:
        apiVersion: apps.kio.kasten.io/v1alpha1
        kind:       RestorePoint
-       name:       ${backup}
-       namespace:  kasten-io
+       namespace:  ${ns}
+       name:       ${backup_name}
   params:
     targetNamespace: ${target}
     pointInTime: 2030-01-01T00:00:00Z
 EOF
 
-kubectl get restoreaction.actions.kio.kasten.io -oyaml
+restore_name=$(kubectl get restoreactions -n "${ns}" -ojson | jq -r ' .items | sort_by(.metadata.creationTimestamp) | .[-1] | .metadata.name ')
 
+state=""
+while [[ "${state}" != "Passed" ]] && [[ "${state}" != "Failed" ]]
+do
+    sleep 3
+    state=$(kubectl get restoreactions -n "${ns}" "${restore_name}" -ojson | jq -r '.status.state')
+done
+
+if [[ "${state}" == "Failed" ]]
+then
+    exit 1
+fi

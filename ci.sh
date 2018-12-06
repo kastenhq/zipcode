3b0dbffbb1057429bed3a647cdbb106b6f74b959
Tom Manville <tom@kasten.io>
2018-12-05 18:02:54 -0800

    Use shippable kubectl

--- ci.sh
+++ ci.sh
@@ -9,6 +9,7 @@ set -o xtrace
 set +o xtrace
 echo -e "${ZIPCODEK8SCFG_INTEGRATION_KUBECONFIGCONTENT}" > kubectl_cfg
 set -o xtrace
+export KUBECONFIG=${SHIPPABLE_BUILD_DIR}/kubectl_cfg
 
 export KUBECONFIG=kubectl_cfg
 export GO111MODULE=on
@@ -19,10 +20,9 @@ export kubectl_cmd=( docker run --rm -ti -p 5432 -p 9000-9030 -p 41134 --volumes
 go mod download
 go build -v ./cmd/zipcode
 
-
-${helm_cmd[@]} init --upgrade --force-upgrade --wait
-${helm_cmd[@]} repo add kanister https://charts.kanister.io/
-${helm_cmd[@]} install kanister/kanister-postgresql \
+#${helm_cmd[@]} init --upgrade --force-upgrade --wait
+#${helm_cmd[@]} repo add kanister https://charts.kanister.io/
+#${helm_cmd[@]} install kanister/kanister-postgresql \
 #-n "zipcode-${BRANCH}-${BUILD_NUMBER}" \
 #--namespace "postgresql-${BRANCH}-${BUILD_NUMBER}" \
 #--set postgresDatabase=zipcode \
@@ -34,10 +34,13 @@ sleep 20
 
 #./restore.sh "postgresql-${BRANCH}-${BUILD_NUMBER}"
 
-${kubectl_cmd[@]} get all -n "postgresql-${BRANCH}-${BUILD_NUMBER}"
-${kubectl_cmd[@]} get pods --selector=app="zipcode-${BRANCH}-${BUILD_NUMBER}-kanister-postgresql" -n "postgresql-${BRANCH}-${BUILD_NUMBER}"
+#${kubectl_cmd[@]} get all -n "postgresql-${BRANCH}-${BUILD_NUMBER}"
+#${kubectl_cmd[@]} get pods --selector=app="zipcode-${BRANCH}-${BUILD_NUMBER}-kanister-postgresql" -n "postgresql-${BRANCH}-${BUILD_NUMBER}"
 #${kubectl_cmd[@]} port-forward $(${kubectl_cmd[@]} get pods --selector=app="zipcode-${BRANCH}-${BUILD_NUMBER}-kanister-postgresql" -n "postgresql-${BRANCH}-${BUILD_NUMBER}" --output=jsonpath={.items..metadata.name}) -n "postgresql-${BRANCH}-${BUILD_NUMBER}" 5432:5432 &
-${kubectl_cmd[@]} port-forward $(${kubectl_cmd[@]} get pods --selector=app=postgresql-kanister-postgresql -n zipcode --output=jsonpath={.items..metadata.name}) -n "postgresql-${BRANCH}-${BUILD_NUMBER}" 5432:5432 &
+kubectl get pods --selector=app=postgresql-kanister-postgresql -n zipcode --output=jsonpath={.items..metadata.name}
+kubectl port-forward $(kubectl get pods --selector=app=postgresql-kanister-postgresql -n zipcode --output=jsonpath={.items..metadata.name}) -n "postgresql-${BRANCH}-${BUILD_NUMBER}" 5432:5432 &
+
+sleep 3
 
 for fi in $(ls env/); do export $fi=$(cat env/$fi); done
 
